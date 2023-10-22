7d58618e482d21de7fbcf4c34932f1088106b6ab
Илья Попов <ilapopov@MacBook-Air-Ila.local>
2023-10-22 17:35:42 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let myLeads = []
+const inputEl = document.querySelector('#input-el')
+const inputBtn = document.querySelector('#input-btn')
+const ulEl = document.querySelector('#ul-el')
+
+inputBtn.addEventListener('click', () => {
+    myLeads.push(inputEl.value)
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ''
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += '<li>' + myLeads[i] + '</li>'
+    }
+    ulEl.innerHTML = listItems
+}
