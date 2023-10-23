92137c294d29d80545afe1f4298466063a510a69
Илья Попов <ilapopov@MacBook-Air-Ila.local>
2023-10-23 16:59:37 +0300

    add functionality to all buttons

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ if (leadsFromLocalStorage) {
     render(myLeads)
 }
 
-const tabs = [
-    {url: 'www.leningradspb.ru'}
-]
-
 tabBtn.addEventListener('click', () => {
-    console.log(tabs[0].url)
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem('myLeads', JSON.stringify(myLeads))
+        render(myLeads)
+    })
 })
 
 function render(leads) {
